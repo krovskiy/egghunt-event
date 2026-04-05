63ea961aef6b264536dff3edee613b6bcaffe585
dima <168024863+krovskiy@users.noreply.github.com>
2026-04-06 00:51:28 +0300

    feat: add pages

--- server.py
+++ server.py
@@ -6,5 +6,17 @@ app = Flask(__name__, static_folder='./src', static_url_path='/', template_folde
 def index():
     return render_template("index.html")
 
+@app.route("/rules")
+def rules():
+    return render_template("/rules/index.html")
+
+@app.route("/create-egg")
+def create_egg():
+    return render_template("/create-egg/index.html")
+
+@app.route("/my-eggs")
+def my_eggs():
+    return render_template("/my-eggs/index.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
